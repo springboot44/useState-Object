a7b8a7ff7dbebfc0061ccc4b40d7fa8e1fc2d69e
springboot44 <springlearning44@gmail.com>
2025-04-06 15:05:55 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [allvalues,Setallvalues]=useState({firstname:"yoshitha",lastname:"jain"})
+
+  const [name,setName]=useState()
+
+  const handleClick=()=>{
+    Setallvalues({firstname:name})
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div>
+
+
+ <h1>hello myfirst nameis :{allvalues.firstname}</h1>
+ <h1>my lastname is:{allvalues.lastname}</h1>
+
+
+  <input
+
+  type='text'
+  placeholder='enter your name'
+  value={name}
+
+  
+  onChange={(e)=>setName(e.target.value)}
+  />
+
+ <button onClick={handleClick}>Update</button>
+
+  </div>
   )
 }
 
